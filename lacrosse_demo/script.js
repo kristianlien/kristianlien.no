7c2891dbb200643d3bc44c4e9d506beaa0fbac85
Kristian Lien <53395696+kristianlien@users.noreply.github.com>
2024-01-17 10:56:33 +0100

    Add files via upload

---
+++ lacrosse_demo/script.js
@@ -0,0 +1,499 @@
+
+
+var oslokommuner = [
+    "Alna",
+    "Bjerke",
+    "Frogner",
+    "Gamle Oslo",
+    "Grorud",
+    "Grünerløkka",
+    "Nordre Aker",
+    "Nordstrand",
+    "Sagane",
+    "St. Hanshaugen",
+    "Stovner",
+    "Søndre Nordstrand",
+    "Ullern",
+    "Vestre Aker",
+    "Østensjø",
+];
+
+var vikenkommuner = [
+    "Asker",
+    "Bærum",
+    "Drammen",
+    "Eidsvoll",
+    "Flesberg",
+    "Flå",
+    "Fredrikstad",
+    "Gjerdrum",
+    "Halden",
+    "Hobøl",
+    "Hol",
+    "Hurdal",
+    "Indre Østfold",
+    "Jevnaker",
+    "Kongsberg",
+    "Lier",
+    "Lillestrøm",
+    "Lørenskog",
+    "Lunner",
+    "Moss",
+    "Nannestad",
+    "Nesodden",
+    "Nittedal",
+    "Nore og Uvdal",
+    "Rakkestad",
+    "Ringerike",
+    "Rælingen",
+    "Råde",
+    "Røyken",
+    "Sigdal",
+    "Svelvik",
+    "Tinn",
+    "Trøgstad",
+    "Tønsberg",
+    "Ullensaker",
+    "Vestby",
+    "Viken",
+    "Ås"
+];
+
+var innlandetkommuner = [
+    "Alvdal",
+    "Dovre",
+    "Eidskog",
+    "Elverum",
+    "Engerdal",
+    "Etnedal",
+    "Folldal",
+    "Gausdal",
+    "Gjøvik",
+    "Gran",
+    "Grue",
+    "Hamar",
+    "Jevnaker",
+    "Kongsvinger",
+    "Lesja",
+    "Lillehammer",
+    "Lom",
+    "Lunner",
+    "Løten",
+    "Nord-Aurdal",
+    "Nord-Fron",
+    "Nord-Odal",
+    "Nordre Land",
+    "Os (Hedmark)",
+    "Os (Innlandet)",
+    "Rendalen",
+    "Ringebu",
+    "Ringsaker",
+    "Rælingen",
+    "Sel",
+    "Skjåk",
+    "Søndre Land",
+    "Sør-Aurdal",
+    "Sør-Fron",
+    "Sør-Odal",
+    "Stange",
+    "Stor-Elvdal",
+    "Sør-Varanger",
+    "Tolga",
+    "Tynset",
+    "Trysil",
+    "Tynset",
+    "Vågå",
+    "Vang (Hedmark)",
+    "Vang (Innlandet)",
+    "Vestre Slidre",
+    "Vestre Toten",
+    "Våler (Hedmark)",
+    "Våler (Innlandet)",
+    "Østre Toten",
+    "Åmot (Hedmark)",
+    "Åmot (Innlandet)",
+];
+
+var møreogromsdalkommuner = [
+    "Ålesund",
+    "Åndalsnes",
+    "Aukra",
+    "Aure",
+    "Averøy",
+    "Eide",
+    "Fræna",
+    "Giske",
+    "Gjemnes",
+    "Haram",
+    "Hareid",
+    "Herøy",
+    "Kristiansund",
+    "Midsund",
+    "Molde",
+    "Nesset",
+    "Norddal",
+    "Orskog",
+    "Ørsta",
+    "Rauma",
+    "Sande",
+    "Sandøy",
+    "Skodje",
+    "Smøla",
+    "Stordal",
+    "Stranda",
+    "Sula",
+    "Sunndal",
+    "Surnadal",
+    "Sykkylven",
+    "Tingvoll",
+    "Ulstein",
+    "Vanylven",
+    "Vestnes",
+    "Volda"
+];
+
+var nordlandkommuner = [
+    "Alstahaug",
+    "Andøy",
+    "Ballangen",
+    "Beiarn",
+    "Bindal",
+    "Bodø",
+    "Brønnøy",
+    "Bø",
+    "Dønna",
+    "Evenes",
+    "Fauske",
+    "Flakstad",
+    "Gildeskål",
+    "Grane",
+    "Hadsel",
+    "Hamarøy",
+    "Hattfjelldal",
+    "Hemnes",
+    "Herøy (Nordland)",
+    "Leirfjord",
+    "Lurøy",
+    "Lødingen",
+    "Meløy",
+    "Moskenes",
+    "Narvik",
+    "Nesna",
+    "Osen",
+    "Rana",
+    "Rødøy",
+    "Røst",
+    "Saltdal",
+    "Sømna",
+    "Sørfold",
+    "Sortland",
+    "Steigen",
+    "Sørfold",
+    "Tjeldsund",
+    "Træna",
+    "Vefsn",
+    "Vega",
+    "Vestvågøy",
+    "Vevelstad",
+    "Værøy",
+    "Øksnes",
+];
+
+var rogalandkommuner = [
+    "Bjerkreim",
+    "Bokn",
+    "Eigersund",
+    "Finnøy",
+    "Gjesdal",
+    "Haugesund",
+    "Hjelmeland",
+    "Hå",
+    "Karmøy",
+    "Klepp",
+    "Kvitsøy",
+    "Lund",
+    "Randaberg",
+    "Rennesøy",
+    "Sandnes",
+    "Sauda",
+    "Sokndal",
+    "Sola",
+    "Stavanger",
+    "Strand",
+    "Suldal",
+    "Time",
+    "Tysvær",
+    "Utsira",
+    "Vindafjord",
+];
+
+var vestfoldogtelemarkkommuner = [
+    "Bamble",
+    "Drangedal",
+    "Fyresdal",
+    "Hjartdal",
+    "Kragerø",
+    "Kviteseid",
+    "Nissedal",
+    "Nome",
+    "Notodden",
+    "Porsgrunn",
+    "Seljord",
+    "Siljan",
+    "Skien",
+    "Tinn",
+    "Tokke",
+    "Vinje",
+    "Færder",
+    "Horten",
+    "Larvik",
+    "Sandefjord",
+    "Holmestrand",
+    "Horten",
+    "Re",
+    "Tønsberg",
+];
+
+var tromsogfinmarkkommuner = [
+    "Alta",
+    "Berlevåg",
+    "Båtsfjord",
+    "Gamvik",
+    "Hasvik",
+    "Hammerfest",
+    "Karlsøy",
+    "Kautokeino",
+    "Kvænangen",
+    "Lebesby",
+    "Loppa",
+    "Måsøy",
+    "Nesseby",
+    "Nordkapp",
+    "Porsanger",
+    "Sør-Varanger",
+    "Tana",
+    "Vadsø",
+    "Vardø",
+    "Balsfjord",
+    "Bardu",
+    "Dyrøy",
+    "Gratangen",
+    "Harstad",
+    "Ibestad",
+    "Kvæfjord",
+    "Lavangen",
+    "Lenvik",
+    "Lyngen",
+    "Målselv",
+    "Narvik",
+    "Salangen",
+    "Skjervøy",
+    "Skånland",
+    "Storfjord",
+    "Tromsø",
+    "Bjarkøy",
+    "Kåfjord",
+    "Sørreisa",
+    "Torsken",
+    "Tranøy",
+];
+
+var trøndelagkommuner = [
+    "Agdenes",
+    "Flatanger",
+    "Fosnes",
+    "Frosta",
+    "Frøya",
+    "Grong",
+    "Hemne",
+    "Hitra",
+    "Holtålen",
+    "Inderøy",
+    "Indre Fosen",
+    "Leka",
+    "Levanger",
+    "Lierne",
+    "Malvik",
+    "Meldal",
+    "Melhus",
+    "Meråker",
+    "Midtre Gauldal",
+    "Namdalseid",
+    "Namsos",
+    "Namsskogan",
+    "Nærøysund",
+    "Oppdal",
+    "Orkdal",
+    "Osen",
+    "Overhalla",
+    "Rennebu",
+    "Rindal",
+    "Røros",
+    "Røyrvik",
+    "Selbu",
+    "Skaun",
+    "Snåsa",
+    "Steinkjer",
+    "Stjørdal",
+    "Trondheim",
+    "Tydal",
+    "Verdal",
+    "Verran",
+    "Vikna",
+];
+
+var vestlandkommuner = [
+    "Askøy",
+    "Aurland",
+    "Austevoll",
+    "Austrheim",
+    "Bergen",
+    "Bjørnafjorden",
+    "Bremanger",
+    "Eidfjord",
+    "Etne",
+    "Fedje",
+    "Fjaler",
+    "Gloppen",
+    "Gulen",
+    "Hyllestad",
+    "Høyanger",
+    "Kinn",
+    "Luster",
+    "Masfjorden",
+    "Modalen",
+    "Osterøy",
+    "Samnanger",
+    "Sogndal",
+    "Solund",
+    "Stad",
+    "Stord",
+    "Stryn",
+    "Sunnfjord",
+    "Vaksdal",
+    "Vik",
+    "Voss",
+];
+
+var agderkommuner = [
+    "Arendal",
+    "Birkenes",
+    "Bygland",
+    "Bykle",
+    "Evje og Hornnes",
+    "Farsund",
+    "Flekkefjord",
+    "Gjerstad",
+    "Grimstad",
+    "Hægebostad",
+    "Iveland",
+    "Kristiansand",
+    "Kvinesdal",
+    "Lillesand",
+    "Lindesnes",
+    "Lyngdal",
+    "Risør",
+    "Sirdal",
+    "Tvedestrand",
+    "Valle",
+    "Vegårshei",
+    "Vennesla",
+    "Åmli",
+    "Åseral",
+];
+
+let kommuneEL = document.getElementById("kommune");
+
+
+function populatedropdown(options, select) {
+    kommuneEL.innerHTML = "<option>Velg kommune...</option>";
+
+
+
+    // clearDropdown(select)
+    for (let i = 0; i < options.length; i++) {
+        var kommune = options[i];
+        var el = document.createElement("option");
+        el.textContent = kommune;
+        select.appendChild(el);
+    }
+
+}
+
+function clearDropdown(select) {
+    for (i = 0; i < select.length; i++) {
+        var option = select.options[i];
+        select.remove(option); 
+    }
+}
+
+var select = document.getElementById("kommune"); //liste av kommuner vi må lage selv i select
+var fylke = document.getElementById("banan").value;
+var options = []
+
+
+select.addEventListener("click", refresh)
+function refresh(e) {
+    fylke = document.getElementById("banan").value;
+    if (fylke == "Vestfold og Telemark") {
+        options = vestfoldogtelemarkkommuner
+        populatedropdown(options, select);
+    }
+
+    if (fylke == "Viken") {
+        options = vikenkommuner;
+        populatedropdown(options, select);
+
+    }
+
+    if (fylke == "Innlandet") {
+        options = innlandetkommuner;
+        populatedropdown(options, select);
+    }
+
+    if (fylke == "Møre og Romsdal") {
+        options = møreogromsdalkommuner;
+        populatedropdown(options, select);
+    }
+
+    if (fylke == "Nordland") {
+        options = nordlandkommuner;
+        populatedropdown(options, select);
+    }
+
+    if (fylke == "Oslo") {
+        options = oslokommuner;
+        populatedropdown(options, select);
+    }
+
+    if (fylke == "Rogaland") {
+        options = rogalandkommuner;
+        populatedropdown(options, select);
+
+    }
+
+    if (fylke == "Troms og Finmark") {
+        options = tromsogfinmarkkommuner;
+        populatedropdown(options, select);
+    }
+
+    if (fylke == "Trøndelag") {
+        options = trøndelagkommuner;
+        populatedropdown(options, select);
+    }
+
+    if (fylke == "Vestland") {
+        options = vestlandkommuner;
+        populatedropdown(options, select);
+    }
+
+    if (fylke == "Agder") {
+        options = agderkommuner;
+        populatedropdown(options, select);
+    }
+
+
+
+
+}
+
